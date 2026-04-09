3b40e8c8f6788a89f79d89432d870de1c5cbaea8
Luyang Hu <huluyang@seas.upenn.edu>
2026-04-08 23:36:09 -0400

    update: replace favicon with new RoSHI logo

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 html_show_sourcelink = False
-html_favicon = "_static/favicon.svg"
+html_favicon = "_static/favicon.png"
 
 html_sidebars = {
     "index": [],
